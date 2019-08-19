cd8af34a5148ec3c252b73b71b1fa425b0543cf3
Marcus <54276056+individualtool@users.noreply.github.com>
2019-08-19 16:46:12 -0500

    Add files via upload

---
+++ VehicleMarcus.java
@@ -0,0 +1,34 @@
+class Vehicle {
+	int passengers; // number of passengers 
+	int fuelcap;    // fuel capacity in gallons
+	int mpg;		// fuel consumption in gallons
+	int doors;      // numbers of doors
+	int tiresize;  // size of tires in inches
+	String color;   // color of car
+	int weight;     // weight of car in lbs
+	int year;       // year car was made
+	int totalmiles; // mileage car has
+	
+
+		Vehicle() {
+		 passengers = 4;
+		 fuelcap = 20;
+		 mpg = 10;
+		 doors = 4;
+		 tiresize = 20;
+		 weight = 7000;
+		 year = 2006;
+		}
+	    }
+			
+	class VehicleMarcus {
+		public static void main(String[] args){
+	    Vehicle camaro = new Vehicle();
+		
+		System.out.println(camaro.passengers);
+		
+		}
+}
+//No clue what else was going on
+		
+	
