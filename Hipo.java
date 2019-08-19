a54bab3154a07044685a164afe4fba775e29d3d3
knford01 <54276167+knford01@users.noreply.github.com>
2019-08-19 16:12:56 -0500

    Add files via upload

---
+++ Hipo.java
@@ -0,0 +1,22 @@
+public class Hipo {
+	
+	static String hipo = 
+"      (^----^)\n" + 
+"       | ' '|\n " +
+"      |  @ |\n " + 
+"     (  --- ).....\"Hipo FTW\"\n" +
+"  EO   `[```]`   O3 \n" + 
+"  ||  (      )   ||\n" +
+"  ||==| .  . |===||\n" + 
+"      |   .  | \n" +
+"     _|     _|\n" + 
+"    (  (    |\n" +
+"      |  |  |\n" +
+"      |  |  |\n" +
+"      |  |  |\n" +
+"      |__>__>\n";
+
+	public static void main(String[] args) {
+		System.out.println(hipo);
+	}
+}
