e9108ff9a7f20d4dcbc1c43fd304817734a53fe5
aapucke <54281045+aapucke@users.noreply.github.com>
2019-08-19 12:56:11 -0700

    Add files via upload

---
+++ vehicle.java
@@ -0,0 +1,43 @@
+class Vehicle{
+	
+		int passengers; // How many passengers
+		int fuelcap;    // fuel capacity
+		int mpg ;        //miles per gallon
+		int doors;      // number of doors
+		String color;   // color of cords
+		int tonnage;    // weight of car
+		int year;       // year car made
+//		int range;	     // how m any miles per tank
+			
+	
+	double refill(double gallons) {
+		return (double)  gallons / fuelcap;
+		
+	
+	
+	}
+}
+
+class carinfo{
+	public static void main (String[] args){
+		Vehicle sportscar = new Vehicle();
+		double gallons = 21;
+		int dist = 252;
+		double refill;
+		
+		sportscar.passengers = 2;
+		sportscar.fuelcap = 12;
+		sportscar.mpg = 28;
+		sportscar.doors = 2;
+		sportscar.tonnage = 2;
+		sportscar.year = 2019;
+	//	sportscar.range = 252;
+		
+		refill = sportscar.refill(gallons);
+		
+		
+		System.out.println("You will need to refill" + refill + "times" );
+			
+		
+	}	
+}
