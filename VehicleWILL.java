b8033c68909ebe61b4c2622462e4f06b16084a9b
willwilson1990 <54276192+willwilson1990@users.noreply.github.com>
2019-08-20 11:12:27 -0500

    Add files via upload

--- VehicleWILL.java
+++ VehicleWILL.java
@@ -3,42 +3,79 @@ class Vehicle
 	int passengers; //number of passengers
 	int fuelcap; //ammount of fuel vehicle can hold in gallons
 	int mpg; //miles per gallon
-	int distance; //miles traveled
+	int doors; //number of doors
+	int tiresize; //tire size
+	String color; //color of vehicle
+	int weight; //weight of vehicle
+	int year; // year the car was made
+	int totalmiles; //mileage of car
 	
-//Below is the constructor for vehicals
-	Vehicle (int p, int f, int m, int d)
+//Below is the constructor for vehicles
+	Vehicle (int p, int f, int m, int d, int drs, int t, String c, int w, int y, int tm)
 	{
 		passengers = p;
-		mpg = m;
 		fuelcap = f;
-		distance = d;
+		mpg = m;
+		doors = drs;
+		tiresize = t;
+		color = c;
+		weight = w;
+		year = y;
+		totalmiles = tm;
 	}
-	
-	//return the range
-		int range (int range)
+	// Constructor with 3 parameters
+		Vehicle ( int p, int f, int m)
 		{
-			return mpg * fuelcap;
+			passengers = p;
+			fuelcap = f;
+			mpg = m;
+			doors = 4;
+			tiresize = 56;
+			color = "blue";
+			weight = 5000 ;
+			year = 2009;
+			totalmiles = 50000;
 		}
-		
-		//compute fuel needed for given distance
-			double fuelneeded (int miles) 
+		//Constructor for when values aren't assigned
+			Vehicle ()
 			{
-				return (double) miles / mpg;
+				passengers = 4 ;
+				fuelcap = 12 ;
+				mpg = 23 ; 
+				doors = 4;
+				tiresize = 56 ;
+				color = "blue" ;
+				weight = 5000;
+				year = 2009 ;
+				totalmiles = 50000;
 			}
 			
-			//number of times vehicle needs to refill
-				int refillneeded (int miles)
+			//method to return the range
+				int range (int range)
 				{
-					return (int) distance / mpg / fuelcap;
+					return mpg * fuelcap;
 				}
+				
+				//method to compute fuel needed for given distance
+					double fuelneeded (int miles) 
+					{
+						return (double) miles / mpg;
+					}
+					
+					//method to compute number of times vehicle needs to refill
+						int refillneeded (int miles)
+						{
+							return (int) 252 / mpg / fuelcap;
+						}
 }
 
 class VehConsDemo
 {
 	public static void main (String [] args) //construct vehicles
 	{
-		Vehicle minivan = new Vehicle (7, 16, 21, 252);
-		Vehicle sportscar = new Vehicle (2, 14, 12, 252);
+		Vehicle minivan = new Vehicle (7, 16, 21);
+		Vehicle sportscar = new Vehicle (2, 14, 12);
+		Vehicle sedan = new Vehicle ();
 		double gallons;
 		double gasrefill;
 		int dist = 252;
@@ -50,5 +87,9 @@ class VehConsDemo
 	gallons = sportscar.fuelneeded (dist);
 	gasrefill = sportscar.refillneeded (dist);
 	System.out.println ("To go " + dist + " miles the sportscar needs " + gallons + " gallons of fuel. They will need to refuel " + gasrefill + " times.");
-    }
+   
+	gallons = sedan.fuelneeded (dist);
+	gasrefill = sedan.refillneeded (dist);
+	System.out.println ("To go " + dist + " miles the sedan needs " + gallons + "gallons of fuel. They will need to refuel " + gasrefill + " times.");
+   }
 }
