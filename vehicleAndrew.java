dfb228789c57494da6cf87ebaa61d854f23174f5
aapucke <54281045+aapucke@users.noreply.github.com>
2019-08-20 09:32:44 -0700

    Add files via upload

--- vehicleAndrew.java
+++ vehicleAndrew.java
@@ -7,9 +7,10 @@ class Vehicle{
 		String color;   // color of cords
 		int tonnage;    // weight of car
 		int year;       // year car made
-//		int range;	     // how m any miles per tank
-			
-				Vehicle()
+		int range = fuelcap*mpg;
+		int mileage;// how many miles per tank
+		int doors;	
+			Vehicle()
 		{		
 		passengers = 2;
 		fuelcap = 12;
@@ -17,7 +18,46 @@ class Vehicle{
 		doors = 2;
 		tonnage = 2;
 		year = 2019;
+		tiresize = 35 ;
+		mileage = 0;
+		color = Blue;
+		range = fuelcap*mpg;
 		}
+		
+			Vehicle (int p, int fc,int mpg)
+		{
+		passengers = p;
+		fuelcap = fc;
+		mpg = mpg;
+		doors = drs;
+		tonnage = ton;
+		year = yr;
+		tiresize = ts ;
+		mileage = mlg;
+		color = Blue;
+		range = fuelcap*mpg;
+		
+		}
+				
+				
+			Vehicle(int p,int fc ,int mpg ,int drs ,int ton,int yr,int drs,int mlg,String clr)
+		{
+		
+		passengers = p;
+		fuelcap = fc;
+		mpg = mpg;
+		doors = drs;
+		tonnage = ton;
+		year = yr;
+		tiresizes = trs ;
+		mileage = mlg;
+		color = clr;
+		range = fuelcap*mpg;
+		
+		
+		
+		}
+		
 	
 	double refill(double gallons) {
 		return (double)  gallons / fuelcap;
@@ -45,7 +85,7 @@ class vehicleAndrew{
 		sportscar.doors = 2;
 		sportscar.tonnage = 2;
 		sportscar.year = 2019;
-	//	sportscar.range = 252;
+		sportscar.range = 252;
 		
 		refill = sportscar.refill(gallons);
 		
