3a0e9a107a1d64f923cf0e5f3ca01c41fe626312
lmatyja148 <54276185+lmatyja148@users.noreply.github.com>
2019-08-21 13:35:45 -0500

    Update vehicleLUKE.java

--- vehicleLUKE.java
+++ vehicleLUKE.java
@@ -161,9 +161,9 @@ public boolean startCar(String newKey) {
 			   String hornSound = " aruuuga " ;
 			    
 			  double gallons;
-			   double refillnumber;
-			    int dist = 252; 
-			     gallons = minivan.fuelneeded(dist); 
+		       double refillnumber;
+		   int dist = 252; 
+	       gallons = minivan.fuelneeded(dist); 
 				 
 				
 				 
@@ -173,12 +173,12 @@ public boolean startCar(String newKey) {
 			   System.out.println(" to go "  +  dist  +  " miles minivan needs " +  gallons +  " gallons of fuel. "  );
 				gallons = sportscar.fuelneeded(dist);
 			
-			     System.out.println(" To go " + dist +  " miles sportscar needs " +  gallons + " gallons of fuel. ") ;
-		          gallons = minivan.fuelneeded(dist); 
+			     		System.out.println(" To go " + dist +  " miles sportscar needs " +  gallons + " gallons of fuel. ") ;
+		         	 gallons = minivan.fuelneeded(dist); 
 			
-			        System.out.println (" To go " + dist  + " the minivan will use up " +  minivan.refuel(dist) + " of tank." );
+			      System.out.println (" To go " + dist  + " the minivan will use up " +  minivan.refuel(dist) + " of tank." );
 			
-			         System.out.println (" To go " + dist  + " the sportscar will use up " +  sportscar.refuel(dist) + " of tank.") ;
+			   System.out.println (" To go " + dist  + " the sportscar will use up " +  sportscar.refuel(dist) + " of tank.") ;
 					 
 					   
 					   
@@ -190,9 +190,9 @@ public boolean startCar(String newKey) {
 						
 								System.out.println(minivan.brake()); 
 						
-									System.out.println(minivan.startCar("this key") ? "woot woot you win" : "Bum bum bummmmm you fail"); //CHARLES ADDED
+						           System.out.println(minivan.startCar("this key") ? "woot woot you win" : "Bum bum bummmmm you fail"); //CHARLES ADDED
 
-										System.out.println(minivan.startCar(" 1234 ") ? "woot woot you win" : "Bum bum bummmmm you fail"); //CHARLES ADD
+			  			System.out.println(minivan.startCar(" 1234 ") ? "woot woot you win" : "Bum bum bummmmm you fail"); //CHARLES ADD
 						
 			 
 				
