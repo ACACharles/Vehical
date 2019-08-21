a114751f50fe1b47ed1c75805ffbd53821296eb2
Brian3207 <54276129+Brian3207@users.noreply.github.com>
2019-08-20 22:56:00 -0500

    Add files via upload

--- VehicleBRIAN.java
+++ VehicleBRIAN.java
@@ -15,92 +15,101 @@ class Vehicle {
 	String key;
 	String color;
 	
- 
-	
-	// Constructor
-	//CHARLES SAID:I would like the first 9 instance variables populated
-		//CHARLES SAID: overwriting parameters with instance (you the assingment backwards)
-	Vehicle (int m, int f, int p) {
-		
-		m = mpg;
-		f = fuelcap;
-		p = passengers;
-	}
-	
-	// Constructor
-	//CHARLES SAID: do not need to give data type for instance variables that are already declared
-	Vehicle () {
-	
-	int doors = 4;
-	int tireSize = 34;
-	int tonage = 3500;
-	int modelYear = 1964;
-	int passengers = 4;
-	int fuelcap = 14;
-	int mpg = 12;
-	int totalMiles = 3400;
-	int topSpeed = 130;
-	int speed = 65;
-	int fuelLevel;
-	String horn = "Beep";
-	String key = " Remote ";
-	String color = "Electric Blue";
-	}
-	
-	// Constructor
-	Vehicle(int d, int tS, int w, int y, int p, int f, int m, int tM, String c) 
 	{
-		doors = d;
-		tireSize = tS;
-		tonage = w;
-		modelYear = y;
-		color = c;
-		passengers = p;
-		fuelcap = f;
-		mpg = m;
-		totalMiles = tM; 
-		
-		
-		
+	passengers = p;
+	fuelcap = fC;
+	mpg = m;
+	doors = d;
+	tiresize = tS;
+	tonage = t;
+	modelYear = y;
+	total miles = tM;
+	topSpeed = tS;
+	speed = s;
+	fuelLevel = fL;
+	color = c;
+	horn = h;
+	key = k;
 	}
-		
-	// Methods	
-	int range() 
+	
+	Vehicle () 
 	{
+	doors = 4;
+	tireSize = 34;
+	tonage = 3500;
+	modelYear = 1964;
+	passengers = 4;
+	fuelcap = 14;
+	mpg = 12;
+	totalMiles = 3400;
+	topSpeed = 130;
+	speed = 65;
+	horn = "Beep";
+	key = " Remote ";
+	color = "Electric Blue";
+	}
+	
+
+	int range() {
 		return mpg * fuelcap;
 	}
 	int range (int m, int fC)
+	{	
+	return m * fC;
+	}
+	
+	//Overloaded Range
+	int range (int m, int c)
 	{
-		return m * fC;
-	} 
-	////////	//CHARLES SAID: MISSING FUEL NEEDED METHODS
-	double refillneeded() {
-		
-		int refill = totalMiles/mpg;
-		return refill/fuelcap +1;
-		
+	mpg = m; fuelcap = c;
+	return m * c;
 	}
-	double refillneeded( int tM, int m, int fC) {
-		int refuel = tM/m;
-	return refuel/fC + 1;
+	
+	double fuelneeded(int miles) 
+	{
+	return (double) miles * mpg;
+	}
+	
+	//Overloaded Fuel Needed
+	double fuelneeded (int miles, int m)
+	{
+	mpg = m;
+	return (double) miles / mpg; 
+	}
+	
+	double refillneeded( int miles) 
+	{
+		return miles / range() ;
+	}
+	
+	// Overloaded Refill Needed
+	int refillneeded (int miles, int c, int m)
+	{
+	mpg = m; fuelcap = c; 
+	return (int) 252 / m / c ;
+	}
+	
+	
+	
+	public static String carhorn() 
+	{
+	String carhorn = " honk honk........";
+		return carhorn; 
 	}
 
-	public static String horn() {
-		String motion = " Bang Bang........";
-		return motion; }
-/* CHARLES SAID
-
-	public static String carHorn() {
-		horn = " Bang Bang........";
-		return horn; }	
-*/	
 	public static String accelerate()
-	{ String motion = "I fill the need for speed.....";
-		return motion; }
+	{ 
+	
+	String motion = "Accelerating.....";
+		return motion; 
+	}
 		
 	public static String brake()
-	{ String motion = "Deer....";
-		return motion; }
+	{ 
+	
+	String brake = "Brake Check....";
+		return brake; 
+	}
 	
 	 public boolean display(String USEDkey)
 	{	
@@ -116,34 +125,35 @@ class Vehicle {
 			return false; 
 		}
 		
+	
 	}
+	
+	
 }
 
-class VehicleBRIAN {
+class VehicleBRIAN{
 	
 	public static void main ( String[] args) {
 		Vehicle truck = new Vehicle();
 		Vehicle jeep = new Vehicle();
 	double gallons ;
-	int tM = 252;
+	int dist = 252;
 	
 	truck.passengers = 4 ;
 	truck.fuelcap = 14;
 	truck.mpg = 12;
-	truck.totalMiles = 465;
 	
-	jeep.passengers = 4;
+	jeep.passengers = 2;
 	jeep.fuelcap = 12;
 	jeep.mpg = 6;
-	jeep.totalMiles = 465;
 	
-	gallons = truck.refillneeded();
+	gallons = truck.refillneeded(dist);
 	
-	System.out.println("To go " + tM + " refill truck needs " +  gallons + " gallons of fuel ");
+	System.out.println("To go " + dist + " refill truck needs " +  gallons + " gallons of fuel ");
 	
-	gallons = jeep.refillneeded();
+	gallons = jeep.refillneeded(dist);
 	
-	System.out.println("To go " + tM + " refill jeep needs " +  gallons + " gallons of fuel ");
+	System.out.println("To go " + dist + " refill jeep needs " +  gallons + " gallons of fuel ");
 	
 	}
 }
